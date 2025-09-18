aaf023a08f3ff5ccd1ef6d84ae73c49ed3bcee85
Mehmet Kanat <mehmet.kanat@protonmail.com>
2025-09-18 21:22:08 +0300

    Add value finder by case name

--- src/enum/EisenhowerStatusColor.php
+++ src/enum/EisenhowerStatusColor.php
@@ -14,4 +14,14 @@ enum EisenhowerStatusColor: string
         return ucfirst(strtolower($this->name));
     }
 
+    public static function valueFromName(string $name): ?string
+    {
+        // Since i use PHP 8.2 i need this function but can be done like this at PHP 8.3: EisenhowerStatusColor::{$eisenhowerStatus}->value
+        foreach (self::cases() as $case) {
+            if ($case->name === $name) {
+                return $case->value;
+            }
+        }
+        return null; // not found
+    }
 }
